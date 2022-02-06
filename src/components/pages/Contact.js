dbdfc7c0c8185ab4114c80e9be883908c2f7f0d0
Alexa Isaks <52163328+AlexaIsaks@users.noreply.github.com>
2022-02-06 14:50:04 +0200

    Updated contact page with new linkedin icon.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,3 +1,5 @@
+import LinkedinIcon from "../../assets/images/icons/linkedin-icon.svg";
+
 // Contact page
 const Contact = () => {
   return (
@@ -7,7 +9,8 @@ const Contact = () => {
           <h1 className="contact__title">_Contact</h1>
           <h2 className="contact__subtitle">Let's Connect</h2>
           <a href="https://www.linkedin.com/in/alexa-isaks-9111701ba/" target="_blank" rel="noopener noreferrer" className="contact__link">
-            <ion-icon name="logo-linkedin" className="contact__linkedin-icon"></ion-icon><span>LinkedIn</span>
+            <img src={LinkedinIcon} alt="LinkedIn icon" className="contact__linkedin-icon"/>
+            <h3 className="contact__linkedin-title">LinkedIn</h3>           
           </a>
         </div>
       </div>

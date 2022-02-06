666e0eedb00a2efe46080a9c9488ad6098ddcc58
Alexa Isaks <52163328+AlexaIsaks@users.noreply.github.com>
2022-02-06 19:52:08 +0200

    Add floral image to contact page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,12 +6,23 @@ const Contact = () => {
     <main className="site__main--height">
       <div className="contact">
         <div className="contact-inner">
-          <h1 className="contact__title">_Contact</h1>
-          <h2 className="contact__subtitle">Let's Connect</h2>
-          <a href="https://www.linkedin.com/in/alexa-isaks-9111701ba/" target="_blank" rel="noopener noreferrer" className="contact__link">
-            <img src={LinkedinIcon} alt="LinkedIn icon" className="contact__linkedin-icon"/>
-            <h3 className="contact__linkedin-title">LinkedIn</h3>           
-          </a>
+          <div className="contact__contact-container">
+            <h1 className="contact__title">_Contact</h1>
+            <h2 className="contact__subtitle">Let's Connect</h2>
+            <a
+              href="https://www.linkedin.com/in/alexa-isaks-9111701ba/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="contact__link"
+            >
+              <img
+                src={LinkedinIcon}
+                alt="LinkedIn icon"
+                className="contact__linkedin-icon"
+              />
+              <h3 className="contact__linkedin-title">LinkedIn</h3>
+            </a>
+          </div>
         </div>
       </div>
     </main>

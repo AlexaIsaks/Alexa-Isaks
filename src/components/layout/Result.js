f56679437b8af2f1ef393998a617c1be749a5169
Alexa Isaks <52163328+AlexaIsaks@users.noreply.github.com>
2022-02-23 22:27:23 +0200

    Include project image in link.

--- src/components/layout/Result.js
+++ src/components/layout/Result.js
@@ -1,16 +1,24 @@
 import Github from "../../assets/images/icons/github-icon.svg";
 
 // Portfolio result
-const Result = ({project}) => {
+const Result = ({ project }) => {
   return (
     <div className={`result ${project.tags.join(" ")}`}>
-      <div className="result__image-container">
-        <img src={require(`../../assets/images/portfolio/${project.image}`).default} alt={project.title} className="result__image" />
-      </div>
-
       <a href={project.link} target="_blank" rel="noopener noreferrer" className="result__link">
-        <img src={Github} alt="GitHub icon" className="result__github" />
-        <p className="result__title">{project.title}</p>
+        <div className="result__image-container">
+          <img
+            src={
+              require(`../../assets/images/portfolio/${project.image}`).default
+            }
+            alt={project.title}
+            className="result__image"
+          />
+        </div>
+
+        <div className="result__github-container">
+          <img src={Github} alt="GitHub icon" className="result__github" />
+          <p className="result__title">{project.title}</p>
+        </div>
       </a>
     </div>
   );

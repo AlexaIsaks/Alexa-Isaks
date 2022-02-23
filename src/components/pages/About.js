ac6f007ca03990442dd55a279a77a796dc484a1a
Alexa Isaks <52163328+AlexaIsaks@users.noreply.github.com>
2022-02-23 22:41:07 +0200

    Updated the about page info

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ const About = () => {
   return (
     <main className="site__main">
       <div className="about">
-         {/*Profile picture*/}
+        {/*Profile picture*/}
         <section className="about__profile-picture-container">
           <div className="about__profile-picture"></div>
         </section>
@@ -13,8 +13,15 @@ const About = () => {
           <h1 className="about__title">_About</h1>
           <h2 className="about__subtitle">Hello, I'm so pleased you're here</h2>
           <p className="about__description">
-            I have completed the Immersive Full Stack Web and Software
-            Engineer bootcamp at HyperionDev. I am currently a Software Engineering Graduate Intern at HyperionDev.
+            I have recently completed the Immersive Full Stack Web and Software
+            Engineer bootcamp at HyperionDev. With a background in Science, my
+            analytical skills have strengthened. These skills were essential in
+            delivering quality science results and have been beneficial to me as
+            a developer. I am a constant learner, dedicated to gaining knowledge
+            and understanding my work. It is rewarding to provide solutions to
+            different technical problems. My goal is to always produce work that
+            gives the best web performance and user experience, while following
+            the best coding practices.
           </p>
         </section>
       </div>
